b0400f9a0220cf53036cc946fde1a4aec12dec7c
ilandy <w9874123@gmail.com>
2018-01-03 18:00:49 +0800

    dyson

---
+++ dyson/javascripts/site.js
@@ -0,0 +1,57 @@
+﻿// //**************************************
+// //  3Wcreative Web Design
+// //  https://www.facebook.com/3wcreative
+// //  Updated: 2017/10/19
+// //**************************************
+// var mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+// var Pad = /iPad/i.test(navigator.userAgent);
+// $(document).ready(function() {
+// 	if (mobile){
+// 		$(".noscriptCode").html("<noscript><div><img alt='DCSIMG' id='DCSIMG' width='1' height='1' src='http://weblog.fetnet.net/dcs6rwoj6vd0ousspd9j1urky_4d5v/njs.gif?dcsuri=/nojavascript&amp;WT.js=No&amp;WT.tv=8.6.2'/></div></noscript>");
+// 		$('meta[name="description"]').attr('content','創新大進擊系列首波主打客戶專屬免費優惠。無論您是白金會員、老客戶、遠傳用戶，都能享受吃喝玩樂超多優惠，用手機就能領各項優惠，因為當遠傳用戶就是比較好！');
+// 		$("meta[property='og:description']").attr('content','創新大進擊系列首波主打客戶專屬免費優惠。無論您是白金會員、老客戶、遠傳用戶，都能享受吃喝玩樂超多優惠，用手機就能領各項優惠，因為當遠傳用戶就是比較好！');
+// 	}else{
+// 		$(".noscriptCode").html("<noscript><div><img alt='DCSIMG' id='DCSIMG' width='1' height='1' src='http://weblog.fetnet.net/dcs4r0of5vd0ousspd9j1urky_5d2y/njs.gif?dcsuri=/nojavascript&amp;WT.js=No&amp;WT.tv=8.6.2'/></div></noscript>");
+// 		$('meta[name="description"]').attr('content','創新大進擊系列首波主打客戶專屬免費優惠。無論您是白金會員、老客戶、遠傳用戶，都能享受吃喝玩樂超多優惠，因為當遠傳用戶就是比較好！');
+// 		$("meta[property='og:description']").attr('content','創新大進擊系列首波主打客戶專屬免費優惠。無論您是白金會員、老客戶、遠傳用戶，都能享受吃喝玩樂超多優惠，因為當遠傳用戶就是比較好！');
+// 	}
+// 	//**************************************
+// 	//  window scroll
+// 	//**************************************
+//     $(window).scroll(function (e) {
+//     	var afterScroll = $(window).scrollTop()
+//     	if (mobile) {
+//         	if ($(this).scrollTop() > 0 && (($(document).height() - $('.ch-footer').height()) > afterScroll)) {
+//         		$('.backtop').fadeIn(200);
+//         	} else {
+//         		$('.backtop').fadeOut(200);
+//         	}
+//     	} else {
+//         	if ($(this).scrollTop() > 150) {
+//         		$('.backtop').fadeIn();
+//         	} else {
+//         		$('.backtop').fadeOut();
+//         	}
+//     	}
+//     });
+// 	$(".backtop a").click(function() {
+//         $("html, body").animate({ scrollTop: 0 }, "slow");
+// 	});
+//     wow = new WOW({
+//         //mobile: false,
+//     })
+//     wow.init();
+// });
+// //**************************************
+// //  window resize
+// //**************************************
+// $(window).resize(function (){
+// });
+// //**************************************
+// //  window load
+// //**************************************
+// $(window).load(function(){
+//     window.onunload = function (){window.scrollTo(0, 0);}
+// });
+
+
