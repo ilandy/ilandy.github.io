092eb442c98780694da7d0fb1afe257995e3ed6b
ilandy <w9874123@gmail.com>
2018-01-09 11:45:15 +0800

    完成文章

--- dyson/javascripts/site.js
+++ dyson/javascripts/site.js
@@ -1,57 +1,50 @@
-﻿// //**************************************
-// //  3Wcreative Web Design
-// //  https://www.facebook.com/3wcreative
-// //  Updated: 2017/10/19
-// //**************************************
-// var mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-// var Pad = /iPad/i.test(navigator.userAgent);
-// $(document).ready(function() {
-// 	if (mobile){
-// 		$(".noscriptCode").html("<noscript><div><img alt='DCSIMG' id='DCSIMG' width='1' height='1' src='http://weblog.fetnet.net/dcs6rwoj6vd0ousspd9j1urky_4d5v/njs.gif?dcsuri=/nojavascript&amp;WT.js=No&amp;WT.tv=8.6.2'/></div></noscript>");
-// 		$('meta[name="description"]').attr('content','創新大進擊系列首波主打客戶專屬免費優惠。無論您是白金會員、老客戶、遠傳用戶，都能享受吃喝玩樂超多優惠，用手機就能領各項優惠，因為當遠傳用戶就是比較好！');
-// 		$("meta[property='og:description']").attr('content','創新大進擊系列首波主打客戶專屬免費優惠。無論您是白金會員、老客戶、遠傳用戶，都能享受吃喝玩樂超多優惠，用手機就能領各項優惠，因為當遠傳用戶就是比較好！');
-// 	}else{
-// 		$(".noscriptCode").html("<noscript><div><img alt='DCSIMG' id='DCSIMG' width='1' height='1' src='http://weblog.fetnet.net/dcs4r0of5vd0ousspd9j1urky_5d2y/njs.gif?dcsuri=/nojavascript&amp;WT.js=No&amp;WT.tv=8.6.2'/></div></noscript>");
-// 		$('meta[name="description"]').attr('content','創新大進擊系列首波主打客戶專屬免費優惠。無論您是白金會員、老客戶、遠傳用戶，都能享受吃喝玩樂超多優惠，因為當遠傳用戶就是比較好！');
-// 		$("meta[property='og:description']").attr('content','創新大進擊系列首波主打客戶專屬免費優惠。無論您是白金會員、老客戶、遠傳用戶，都能享受吃喝玩樂超多優惠，因為當遠傳用戶就是比較好！');
-// 	}
-// 	//**************************************
-// 	//  window scroll
-// 	//**************************************
-//     $(window).scroll(function (e) {
-//     	var afterScroll = $(window).scrollTop()
-//     	if (mobile) {
-//         	if ($(this).scrollTop() > 0 && (($(document).height() - $('.ch-footer').height()) > afterScroll)) {
-//         		$('.backtop').fadeIn(200);
-//         	} else {
-//         		$('.backtop').fadeOut(200);
-//         	}
-//     	} else {
-//         	if ($(this).scrollTop() > 150) {
-//         		$('.backtop').fadeIn();
-//         	} else {
-//         		$('.backtop').fadeOut();
-//         	}
-//     	}
-//     });
-// 	$(".backtop a").click(function() {
-//         $("html, body").animate({ scrollTop: 0 }, "slow");
-// 	});
-//     wow = new WOW({
-//         //mobile: false,
-//     })
-//     wow.init();
-// });
-// //**************************************
-// //  window resize
-// //**************************************
-// $(window).resize(function (){
-// });
-// //**************************************
-// //  window load
-// //**************************************
-// $(window).load(function(){
-//     window.onunload = function (){window.scrollTo(0, 0);}
-// });
+﻿//**************************************
+//  3Wcreative Web Design
+//  https://www.facebook.com/3wcreative
+//  Updated: 2017/10/19
+//**************************************
+var mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+var Pad = /iPad/i.test(navigator.userAgent);
+$(document).ready(function() {
+	//**************************************
+	//  window scroll
+	//**************************************
+    
+	$(".backTop").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+	});
+    wow = new WOW({
+        //mobile: false,
+    })
+    wow.init();
 
+    /* ilandy */
+   $(window).on('scroll',function (){
+        if( $(this).scrollTop() > 500) {
+            $('.backTop').show();
+        } else {
+            $('.backTop').hide();
+        }
+    });
+    $('.nav-list').find('.btn').on('click',function(){
+        
+        var idx = $(this).index()-1;
+        var pos = $('.panel').eq(idx).offset();
+        
+        $('.nav-mob').toggleClass('active');
+        $('.nav-list').toggleClass('active');
+
+        $("html, body").animate({ scrollTop: pos.top-30 }, "slow");
+        
+    });
+    $('.nav-mob').on('click',function (){
+        $(this).toggleClass('active');
+        $('.nav-list').toggleClass('active');
+    });
+    
+});
+
+function linkTo (path){
+    window.location.replace(path);
+}
 
