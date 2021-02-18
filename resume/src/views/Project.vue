e577ab91f261dea63be83799c4cbcac499f4b350
ilandychiu <w9874123@gmail.com>
2021-02-18 14:38:24 +0800

    調整網址文字過長

--- resume/src/views/Project.vue
+++ resume/src/views/Project.vue
@@ -5,8 +5,8 @@
       
       <article>
         <h2 class="color-secendary">旗艦機系列網頁</h2>
-        URL:
-        <a href="https://www.tstartel.com/CWS/iPhone12.php">https://www.tstartel.com/CWS/iPhone12.php</a
+        
+        <a href="https://www.tstartel.com/CWS/iPhone12.php">Web Site URL</a
         ><br />
         Jobs: Design, Design to RWD Web
         <br />
@@ -14,8 +14,8 @@
       </article>
        <article>
         <h2 class="color-secendary">5G網速升級體驗</h2>
-        URL:
-        <a href="https://www.tstartel.com/CWS/upgradeEXP_5G.php">https://www.tstartel.com/CWS/upgradeEXP_5G.php</a
+        
+        <a href="https://www.tstartel.com/CWS/upgradeEXP_5G.php">Web Site URL</a
         ><br />
         Jobs: Design, Design to RWD Web
         <br />
@@ -23,8 +23,8 @@
       </article>
       <article>
         <h2 class="color-secendary">台灣之星 辦加值</h2>
-        URL:
-        <a href="https://www.tstartel.com/CWS/vas_pick.php">https://www.tstartel.com/CWS/vas_pick.php</a
+        
+        <a href="https://www.tstartel.com/CWS/vas_pick.php">Web Site URL</a
         ><br />
         Jobs: Design, Design to RWD Web
         <br />
@@ -32,8 +32,8 @@
       </article>
       <article>
         <h2 class="color-secendary">台灣之星 Header&Footer 改版</h2>
-        URL:
-        <a href="https://www.tstartel.com/CWS/">https://www.tstartel.com/CWS/</a
+        
+        <a href="https://www.tstartel.com/CWS/">Web Site URL</a
         ><br />
         Jobs: Design, Design to RWD Web
         <br />
@@ -41,8 +41,8 @@
       </article>
       <article>
         <h2 class="color-secendary">景點訊號</h2>
-        URL:
-        <a href="https://www.tstartel.com/CWS/signalTheme.php">https://www.tstartel.com/CWS/signalTheme.php</a
+        
+        <a href="https://www.tstartel.com/CWS/signalTheme.php">Web Site URL</a
         ><br />
         Jobs: Design to RWD Web
         <br />
@@ -50,8 +50,8 @@
       </article>
       <article>
         <h2 class="color-secendary">遠傳企業服務網頁</h2>
-        URL:
-        <a href="http://ilandy.github.io/project/EBU/ebu_index.html">http://ilandy.github.io/project/EBU/ebu_index.html</a
+        
+        <a href="http://ilandy.github.io/project/EBU/ebu_index.html">Web Site URL</a
         ><br />
         Jobs: Design to RWD Web
         <br />
@@ -59,8 +59,8 @@
       </article>
       <article>
         <h2 class="color-secendary">遠傳活動網頁</h2>
-        URL:
-        <a href="http://ilandy.github.io/project/happyGoBurn/happyGoBurn.html">http://ilandy.github.io/project/happyGoBurn/happyGoBurn.html</a
+        
+        <a href="http://ilandy.github.io/project/happyGoBurn/happyGoBurn.html">Web Site URL</a
         ><br />
         Jobs: Design to RWD Web
         <br />
@@ -68,8 +68,8 @@
       </article>
       <article>
         <h2 class="color-secendary">親子 Dyson 廣宣頁 </h2>
-        URL:
-        <a href="http://ilandy.github.io/project/dyson/index.html">http://ilandy.github.io/project/dyson/index.html</a
+        
+        <a href="http://ilandy.github.io/project/dyson/index.html">Web Site URL</a
         ><br />
         Jobs: Design to RWD Web
         <br />
@@ -77,8 +77,8 @@
       </article>
       <article>
         <h2 class="color-secendary">嘉義市政府線上陳情系統</h2>
-        URL:
-        <a href="https://callcenter.chiayi.gov.tw/#/index">https://callcenter.chiayi.gov.tw/#/index</a
+        
+        <a href="https://callcenter.chiayi.gov.tw/#/index">Web Site URL</a
         ><br />
         Jobs: Design, Design to RWD Web, Getting and sending data form Web
         API<br />
@@ -86,14 +86,14 @@
       </article>
       <article>
         <h2 class="color-secendary">Moviecaker - 電影社群平台</h2>
-        URL: <a href="http://www.moviecaker">http://www.moviecaker.com</a><br />
+         <a href="http://www.moviecaker">Web Site URL</a><br />
         Jobs: APP UI Design, Web Design, Design to AWD Web, Interactive design,
         Maintain source code and fixed issues.<br />
         Skills: HTML5, Sass, CSS3, jQuery, Git<br />
       </article>
       <article>
         <h2 class="color-secendary">高雄市政府線上即時服務系統</h2>
-        URL: <a href="http://soweb.kcg.gov.tw/">http://soweb.kcg.gov.tw/RWD/</a
+         <a href="http://soweb.kcg.gov.tw/">Web Site URL</a
         ><br />
         Jobs: Design, Design to RWD Web, Getting and sending data form Web
         API<br />
@@ -101,7 +101,7 @@
       </article>
       <article>
         <h2 class="color-secendary">顯經黑膠圖書館</h2>
-        URL: <a href="http://www.manivinyl.">http://www.manivinyl.com/</a><br />
+         <a href="http://www.manivinyl.">Web Site URL</a><br />
         Jobs: Create web view<br />
         Skills: HTML5, Sass, Angular2, Git<br />
         
@@ -126,7 +126,7 @@
       </article>
       <article>
         <h2 class="color-secendary">鑫瑪國際車業店家網頁</h2>
-        URL: <a href="http://xinma.com.tw">http://xinma.com.tw</a><br />
+         <a href="http://xinma.com.tw">Web Site URL</a><br />
         Jobs: Design, Design to AWD HTML, PHP, DNS & Server manager, DB design,
         CMS<br />
         Skills: HTML5, CSS, PHP, jQuery, PHP, mySQL<br />
@@ -134,7 +134,7 @@
       </article>
       <article>
         <h2 class="color-secendary">柏緯鐵工企業官網</h2>
-        URL: <a href="http://www.bothwell.com.tw">http://www.bothwell.com.tw</a
+         <a href="http://www.bothwell.com.tw">Web Site URL</a
         ><br />
         Jobs: Design, Design to RWD HTML<br />
         Skills: HTML5, CSS, Bootstrap, jQuery<br />
@@ -142,21 +142,21 @@
       </article>
       <article>
         <h2 class="color-secendary">自願性節約能源填報系統</h2>
-        URL: Private site<br />
+         Private site<br />
         Jobs: Design, Design to HTML<br />
         Skills: HTML5, CSS, Bootstrap<br />
         
       </article>
       <article>
         <h2 class="color-secendary">Pakikupy 官網型購物網站</h2>
-        URL: [off-line] http://www.pakikupy.com/<br />
+         [off-line] http://www.pakikupy.com/<br />
         Jobs: Design, Design to HTML, PHP, DNS & Server manager, DB design<br />
         Skills: HTML5, CSS, PHP, jQuery, PHP, mySQL<br />
         
       </article>
       <article>
         <h2 class="color-secendary">Spore design 網站</h2>
-        URL: [off-line] http://www.pakikupy.com/<br />
+         [off-line] http://www.pakikupy.com/<br />
         Jobs: Design, Design to HTML, PHP, DNS & Server manager, DB design<br />
         Skills: HTML5, CSS, PHP, jQuery, PHP, mySQL<br />
         
